399de70508d1b633858a2f7f451d9582e4514257
dodokyo <ypd03008@naver.com>
2024-05-06 18:55:17 +0900

    ch5-12

--- components/ui/TodoList.jsx
+++ components/ui/TodoList.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { IoShareSocialOutline } from "react-icons/io5";
 import { useCopyToClipboard } from "usehooks-ts";
+import { IoSearchOutline } from "react-icons/io5";
 
 const TodoList = ({ sharedUserFullName = "", owerUserId = "" }) => {
   const [copiedText, copy] = useCopyToClipboard();
@@ -36,6 +37,38 @@ const TodoList = ({ sharedUserFullName = "", owerUserId = "" }) => {
             </div>
           )}
         </article>
+        <article className=" flex flex-col sm:flex-row gap-4 mt-8">
+          <div className=" flex flex-1 h-[60px]">
+            <input
+              className="
+                p-4
+                flex-1
+                bg-[#F7CB66]
+                border border-black rounded-l-2xl font-bold
+            "
+            ></input>
+            <div
+              className=" w-[60px]
+            flex justify-center items-center
+            bg-black rounded-r-2xl
+            cursor-pointer
+            "
+            >
+              <IoSearchOutline size={40} color="#fff" />
+            </div>
+          </div>
+          <div
+            className="
+            h-[60px] w-[200px]
+            flex justify-center items-center
+            bg-[#7EBB95] border border-black rounded-2xl
+            font-bold cursor-pointer text-[20px]
+          "
+          >
+            New Task
+          </div>
+        </article>
+        <div className="h-[2px] my-10 bg-black"></div>
       </div>
     </section>
   );
